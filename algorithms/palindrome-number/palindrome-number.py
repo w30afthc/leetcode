0a3e1b5c84d4877665afe2064443bd99c5b55e97
Reynold <w30apr@qq.com>
2023-12-28 20:12:22 +0800

    New Problem Solution - "9. Palindrome Number"

---
+++ algorithms/palindrome-number/palindrome-number.py
@@ -0,0 +1,30 @@
+"""
+source : https://leetcode.cn/problems/template/description/
+"""
+from typing import List
+
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        return str(x) == str(x)[::-1]
+
+
+class Solution1:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0: return False
+        n = 0
+        num_list = []
+        while x >= 10**n:
+            num_list.append(x//10**n - x//10**(n+1)*10)
+            n += 1
+        if num_list == num_list[::-1]:
+            return True
+        else:
+            return False
+
+
+if __name__ == "__main__":
+    print(Solution().isPalindrome(121))
+    print(Solution().isPalindrome(-121))
+    print(Solution().isPalindrome(10))
+
