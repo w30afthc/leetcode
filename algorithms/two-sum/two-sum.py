945a2d0320ba926c7fe364cda54542b1b65ffb1f
Reynold <w30apr@qq.com>
2023-12-28 20:09:44 +0800

    Update two-sum problem two solutions

--- algorithms/two-sum/two-sum.py
+++ algorithms/two-sum/two-sum.py
@@ -10,21 +10,27 @@ class Solution:
         for i, num in enumerate(nums):
             if target - num in hashtable:
                 return [hashtable[target - num], i]
-            else:
-                hashtable[nums[i]] = i
-        else:
-            return []
+            hashtable[nums[i]] = i
+        return []
+
+
+class Solution2:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            if target - nums[i] in nums[i+1:]:
+                return [i, nums.index(target-nums[i], i+1)]
+
+
+class Solution1:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []
 
 
 if __name__ == "__main__":
-    # nums = [2,7,11,15]
-    # target = 9
-
-    # nums = [3,2,4]
-    # target = 6
-
-    nums = [3,3]
-    target = 6
-
-    print(Solution().twoSum(nums, target))
-
+    print(Solution().twoSum([2, 7, 11, 15], 9))
+    print(Solution().twoSum([3, 2, 4], 6))
+    print(Solution().twoSum([3, 3], 6))
