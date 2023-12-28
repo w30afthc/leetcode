5eca6cd4e658a07aa6d3fd709d138dc271cd4719
Reynold <w30apr@qq.com>
2023-12-28 20:10:12 +0800

    Update problem solution template

--- algorithms/template/template.py
+++ algorithms/template/template.py
@@ -1,35 +1,15 @@
 """
-source : https://leetcode.cn/problems/template/description/
+source : https://leetcode.cn/problems/palindrome-number/description/
 """
-from typing import List
 
 
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def funcitonName(self, parameter_list):
         pass
 
 
-class Solution1:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashtable = dict()
-        for i, num in enumerate(nums):
-            if target - num in hashtable:
-                return [hashtable[target - num], i]
-            else:
-                hashtable[nums[i]] = i
-        else:
-            return []
-
-
 if __name__ == "__main__":
-    # nums = [2,7,11,15]
-    # target = 9
-
-    # nums = [3,2,4]
-    # target = 6
-
-    nums = [3,3]
-    target = 6
-
-    print(Solution().twoSum(nums, target))
+    print(Solution().funcitonName())
+    print(Solution().funcitonName())
+    print(Solution().funcitonName())
 
