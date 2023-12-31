01eb49319976b48d50b6056b75a2ecc2811ac6cf
Reynold <w30apr@qq.com>
2023-12-31 23:21:01 +0800

    Add New Solution for "48. Rotate Image"

--- algorithms/rotate-image/rotate-image.py
+++ algorithms/rotate-image/rotate-image.py
@@ -5,12 +5,54 @@ from typing import List
 
 
 class Solution:
+    """
+    先上下翻转，再按照左上到右下的对角线翻转，实现顺时针90度翻转
+    """
     def rotate(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
+        n = len(matrix)
+        for i in range(n // 2):
+            for j in range(n):
+                matrix[i][j], matrix[n-1-i][j] = matrix[n-1-i][j], matrix[i][j]
 
-        pass
+        for i in range(n):
+            for j in range(i, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+
+class Solution4:
+    """
+    以矩阵中心为界，将矩阵分为左上、右上、右下、左下四个小矩阵进行旋转
+    """
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        for i in range(n // 2):
+            for j in range((n + 1) // 2):
+                matrix[i][j], matrix[n-1-j][i], matrix[n-1-i][n-1-j], matrix[j][n-1-i] = \
+                    matrix[n-1-j][i], matrix[n-1-i][n-1-j], matrix[j][n-1-i], matrix[i][j]
+
+
+class Solution3:
+    """
+    从外到内一圈一圈的旋转
+    """
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        for i in range(n // 2):
+            for j in range(i, n - 1 - i):
+                temp = matrix[i][j]
+                matrix[i][j] = matrix[n-j-1][i]
+                matrix[n-j-1][i] = matrix[n-i-1][n-j-1]
+                matrix[n-i-1][n-j-1] = matrix[j][n-i-1]
+                matrix[j][n-i-1] = temp
 
 
 class Solution2:
@@ -24,7 +66,7 @@ class Solution2:
             answer.append([])
             for j in range(n):
                 answer[i].append(matrix[n-j-1][i])
-        return answer
+        matrix[:] = answer
 
 
 class Solution1:
@@ -33,14 +75,20 @@ class Solution1:
     """
     def rotate(self, matrix: List[List[int]]) -> None:
         n = len(matrix)
-        matrix2 = [[0 for i in range(n)] for j in range(n)]
+        # matrix_new = [[0 for i in range(n)] for j in range(n)]
+        matrix_new = [[None] * n for i in range(n)]
         for i in range(n):
             for j in range(n):
-                matrix2[j][n-i-1] = matrix[i][j]
-        return matrix2
+                matrix_new[j][n-i-1] = matrix[i][j]
+        matrix[:] = matrix_new
 
 
 if __name__ == "__main__":
-    print(Solution().rotate([[1,2,3],[4,5,6],[7,8,9]]))
-    print(Solution().rotate([[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]))
+    matrix_1 = [[1,2,3],[4,5,6],[7,8,9]]
+    Solution().rotate(matrix_1)
+    print(matrix_1)
+
+    matrix_2 = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+    Solution().rotate(matrix_2)
+    print(matrix_2)
 
