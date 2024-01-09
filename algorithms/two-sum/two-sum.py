f235e541ecd33b753434735a0d4cbf7f9f869fe7
Reynold <w30apr@qq.com>
2024-01-09 16:55:55 +0800

    Add TestCase - "1. Two Sum"

--- algorithms/two-sum/two-sum.py
+++ algorithms/two-sum/two-sum.py
@@ -34,3 +34,4 @@ if __name__ == "__main__":
     print(Solution().twoSum([2, 7, 11, 15], 9))
     print(Solution().twoSum([3, 2, 4], 6))
     print(Solution().twoSum([3, 3], 6))
+    print(Solution().twoSum([0, 0, 3, 4], 0))
