ca0baf5d8a937ba7e726e04ecb05c11592091d96
Reynold <w30apr@qq.com>
2024-01-11 23:37:43 +0800

    New Problem Solution - "557. Reverse Words in a String III"

---
+++ algorithms/reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.py
@@ -0,0 +1,45 @@
+"""
+title : 557. Reverse Words in a String III
+source : https://leetcode.cn/problems/reverse-words-in-a-string-iii/
+"""
+from typing import List
+from collections import deque
+
+
+class Solution:
+    """双端队列
+
+    字母头部插入，单词尾部追加
+    时间复杂度： O(n)
+    空间复杂度： O(n)
+    """
+    def reverseWords(self, s: str) -> str:
+        ans = ""
+        word = ""
+        for i in range(len(s)):
+            if s[i] != " ":
+                word = s[i] + word
+            else:
+                ans = ans + word + " "
+                word = ""
+        ans = ans + word
+        return ans
+
+
+class Solution1:
+    """常规思路
+
+    分割、翻转、再连接
+    时间复杂度： O(n)
+    空间复杂度： O(n)
+    """
+    def reverseWords(self, s: str) -> str:
+        s_list = s.split()
+        s_list = [word[::-1] for word in s_list]
+        return " ".join(s_list)
+
+
+if __name__ == "__main__":
+    print(Solution().reverseWords("Let's take LeetCode contest"))
+    print(Solution().reverseWords("Mr Ding"))
+    print(Solution().reverseWords("A"))
