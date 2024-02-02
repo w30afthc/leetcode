04c687d215902af20984b0f660075f38c85a1198
Reynold <w30apr@qq.com>
2024-02-02 21:54:44 +0800

    Add New Solution - "1578. Minimum Time to Make Rope Colorful"

--- algorithms/minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.py
+++ algorithms/minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.py
@@ -6,9 +6,31 @@ from typing import List
 
 
 class Solution:
+    """双指针
+
+    慢指针指向连续相同颜色气球的开始，快指针试探连续相同颜色气球的结束
+    快指针记录相同颜色气球移除耗时最高的，cost 累加其余耗时
+    时间复杂度： O(n)
+    空间复杂度： O(1)
+    """
+    def minCost(self, colors: str, neededTime: List[int]) -> int:
+        slow, cost = 0, 0
+        max_needed_time = neededTime[slow]
+        for fast in range(1, len(colors)):
+            if colors[slow] == colors[fast]:
+                cost += min(max_needed_time, neededTime[fast])
+                if neededTime[fast] > max_needed_time:
+                    max_needed_time = neededTime[fast]
+            else:
+                slow = fast
+                max_needed_time = neededTime[slow]
+        return cost
+
+
+class Solution1:
     """分组循环
 
-    思路说明
+    找出连续的相同颜色的气球，统计其中较小的时间
     时间复杂度： O(n)
     空间复杂度： O(1)
     """
@@ -28,6 +50,6 @@ class Solution:
 
 
 if __name__ == "__main__":
-    print(Solution().minCost(colors = "abaac", neededTime = [1,2,3,4,5]))
-    print(Solution().minCost(colors = "abc", neededTime = [1,2,3]))
-    print(Solution().minCost(colors = "aabaa", neededTime = [1,2,3,4,1]))
+    print(Solution().minCost(colors="abaac", neededTime=[1,2,3,4,5]))
+    print(Solution().minCost(colors="abc", neededTime=[1,2,3]))
+    print(Solution().minCost(colors="aabaa", neededTime =[1,2,3,4,1]))
