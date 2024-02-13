06e029da6246e2fd0599f40d56e0ce57850ec54b
Reynold <w30apr@qq.com>
2024-02-13 23:41:25 +0800

    Add New Solution - "748. Shortest Completing Word"

--- algorithms/shortest-completing-word/shortest-completing-word.py
+++ algorithms/shortest-completing-word/shortest-completing-word.py
@@ -3,14 +3,30 @@ title : 748. Shortest Completing Word
 source : https://leetcode.cn/problems/shortest-completing-word/
 """
 from typing import List
+from collections import Counter
 
 
 class Solution:
     """模拟
 
-    思路说明
-    时间复杂度： O(n)
-    空间复杂度： O(1)
+    先统计 licensePlate 中字母词频，再遍历字符串数组，依次统计各个字符串的词频
+    利用 Counter 对象相减能选出补全词，再挑选长度最短的
+    时间复杂度： O(n + l + m * 26), n 为 licensePlate 长度，l 为 words 中的是所有字符串长度之和
+    m 为 words 数组的长度
+    空间复杂度： O(26)
+    """
+    def shortestCompletingWord(self, licensePlate: str, words: List[str]) -> str:
+        cnt = Counter(ch.lower() for ch in licensePlate if ch.isalpha())
+        return min((word for word in words if not cnt - Counter(word)), key=len)
+
+
+class Solution1:
+    """模拟
+
+    先统计 licensePlate 中字母词频，然后遍历字符串数组中每个字符串
+    如果存在于列表 license_list 则删除，对于能清空词频列表的字符串，选出最短的
+    时间复杂度： O(n * m), n 为 licensePlate 长度，m 为 words
+    空间复杂度： O(n)
     """
     def shortestCompletingWord(self, licensePlate: str, words: List[str]) -> str:
         shortest = "a" * 16
